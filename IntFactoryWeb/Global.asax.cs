5e589c10208a7074ce64ebf1e778f20e6ef08e74
xiaoxiaomuge <xiaoxiaomuge@163.com>
2016-07-18 16:17:34 +0800

    调整

--- IntFactoryWeb/Global.asax.cs
+++ IntFactoryWeb/Global.asax.cs
@@ -21,7 +21,7 @@ namespace IntFactoryWeb
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
