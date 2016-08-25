74804b31917e7bbd7b2ce37ab25268eae7659485
xiaoxiaomuge <xiaoxiaomuge@163.com>
2016-08-25 10:56:35 +0800

    添加企业站后台

---
+++ IntFactoryWeb/Areas/Manage/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntFactoryWeb.Areas.Manage.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Manage/Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
