4b8b201dbf29775d97d059b61df8a618bb0f671a
xiaoxiaomuge <xiaoxiaomuge@163.com>
2016-08-30 13:49:56 +0800

    优化

---
+++ IntFactoryWeb/Areas/M/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntFactoryWeb.Areas.M.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /M/Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
