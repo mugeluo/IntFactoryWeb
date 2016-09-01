fded72b367b182112f1d4076b46f235f7200701c
song19940906 <2079973693@qq.com>
2016-09-01 15:57:10 +0800

    9.1厂盟企业站手机端

--- IntFactoryWeb/modules/m/scripts/layout.js
+++ IntFactoryWeb/modules/m/scripts/layout.js
@@ -17,6 +17,12 @@
             }
         });
 
+        //头部双击 返回顶部
+        //$("header").dblclick(function () {            
+        //    $('body,html').animate({ scrollTop: 0 }, 300);
+        //    return false;
+        //});
+
         $(".menu-icon").click(function () {
             $(".menu").slideToggle();
         });
--- IntFactoryWeb/modules/m/scripts/pricing.js
+++ IntFactoryWeb/modules/m/scripts/pricing.js
@@ -10,10 +10,10 @@
 
         var money = [{ money1: "￥2,400", money2: "￥4,800", money3: "￥9,200", money4: "￥21,600", money5: "￥39,600" }, { money1: "￥4,800", money2: "￥8,400", money3: "￥16,400", money4: "￥38,400", money5: "￥71,200" }, { money1: "￥7,200", money2: "￥10,200", money3: "￥21,600", money4: "￥51,200", money5: "￥93,600" }]
 
-        $(".price-list table .time").click(function () {
-            var _this = $(this), id = _this.data("id");
+        $(".price-list .time-selected li").click(function () {
+            var _this = $(this), id = _this.data("type");
             if (!_this.hasClass("hover")) {
-                $(".price-list table .time").removeClass("hover");
+                $(".price-list .time-selected li").removeClass("hover");
                 _this.addClass("hover");
             }
             $(".price-list table .money1").html(money[id].money1);
@@ -24,8 +24,6 @@
         });
         
     };
-
-    
 
     module.exports = ObjectJS;
 });
