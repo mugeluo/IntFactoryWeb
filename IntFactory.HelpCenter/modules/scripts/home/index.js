aab705ba1069339cc35e44aac396f099f92083e5
song19940906 <2079973693@qq.com>
2016-09-08 17:56:18 +0800

    9.8帮助中心前台

--- IntFactory.HelpCenter/modules/scripts/home/index.js
+++ IntFactory.HelpCenter/modules/scripts/home/index.js
@@ -7,37 +7,16 @@
     };
 
     ObjectJS.bindEvent = function () {    
-
-        $(".visualization .content-ul li").hover(function () {            
-            ObjectJS.bindItemHover(this, $(".visualization .img-content img"));
+        $(".function li").mouseenter(function () {
+            var _this = $(this);
+            _this.find(".detail").show();
+        });
+        
+        $(".function li .detail").mouseleave(function () {
+            $(this).hide();
         });
 
-        $(".team .content-ul li").hover(function () {                    
-            ObjectJS.bindItemHover(this, $(".team .img-content img"));
-        });
-
-        $(".share .content-ul li").hover(function () {           
-            ObjectJS.bindItemHover(this, $(".share .img-content img"));
-        });
-
-        $(".case-company li .bg").hover(
-            function () {
-                var _this = $(this);
-                _this.find(".company").hide();
-                _this.find(".case-desc").show();
-            },
-            function () {
-                var _this = $(this);
-                _this.find(".company").show();
-                _this.find(".case-desc").hide();
-            }
-        );
-
-        setTimeout(function () {
-            $(".img-load").each(function () {
-                $(this).attr("src",$(this).data("src"));
-            })
-        },1000);
+        
     };
 
     ObjectJS.bindItemHover = function (obj, document) {        
