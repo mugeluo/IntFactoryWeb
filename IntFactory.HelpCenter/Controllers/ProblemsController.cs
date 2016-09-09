5f01bd038bb17fadf182ba7f278c5e735ff3efec
song19940906 <2079973693@qq.com>
2016-09-09 17:31:33 +0800

    9.9帮助中心前台

---
+++ IntFactory.HelpCenter/Controllers/ProblemsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntFactory.HelpCenter.Controllers
+{
+    public class ProblemsController : Controller
+    {
+
+        public ActionResult Problems()
+        {
+            return View();
+        }
+
+        public ActionResult HotProblems()
+        {
+            return View();
+        }
+
+        public ActionResult ProblemsDetail()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ IntFactory.HelpCenter/modules/scripts/problems/problems.js
@@ -0,0 +1,18 @@
+﻿define(function (require, exports, module) {
+    var ObjectJS = {};
+
+    ObjectJS.init = function () {
+        ObjectJS.bindEvent();
+    };
+
+    ObjectJS.bindEvent = function () {
+        $(".category ul li").click(function () {
+            var _this = $(this), id = _this.data("id");
+            if (!_this.hasClass("hover")) {
+                _this.siblings().removeClass("hover");
+                _this.addClass("hover");
+            };           
+        });
+    };
+    module.exports = ObjectJS;
+});
