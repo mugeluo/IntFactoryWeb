0e583d32363a1019f74663d7fe9705c805c11af5
xiaoxiaomuge <xiaoxiaomuge@163.com>
2016-09-09 16:42:39 +0800

    优化

--- IntFactory.HelpCenter/Areas/Manage/Controllers/BaseController.cs
+++ IntFactory.HelpCenter/Areas/Manage/Controllers/BaseController.cs
@@ -10,27 +10,6 @@ namespace IntFactory.HelpCenter.Areas.Manage.Controllers
     [IntFactory.HelpCenter.Common.UserAuthorize]
     public class BaseController : Controller
     {
-        //
-        // GET: /Manage/Base/
-        /// <summary>
-        /// 当前登录用户
-        /// </summary>
-        protected UsersEntity CurrentUser
-        {
-            get
-            {
-                if (Session["ClientManager"] == null)
-                {
-                    return null;
-                }
-                else
-                {
-                    return (UsersEntity)Session["ClientManager"];
-                }
-            }
-            set { Session["ClientManager"] = value; }
-        }
-
         protected Dictionary<string, object> JsonDictionary = new Dictionary<string, object>();
 
     }
